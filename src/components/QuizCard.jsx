454c392e644767a5d3507a326c33e59f1aca73d0
MohammadFakih02 <mohammadfakih025@gmail.com>
2024-12-04 16:44:27 +0200

    added QuizCard component

---
+++ src/components/QuizCard.jsx
@@ -0,0 +1,15 @@
+const QuizCard = ({ quiz }) => {
+    const { name, description, points } = quiz;
+
+    return (
+        <div className="quiz-card">
+            <div>
+                <h3>{name}</h3>
+                <h3>{points}</h3>
+            </div>
+            <div>
+                <p>{description}</p>
+            </div>
+        </div>
+    )
+}
